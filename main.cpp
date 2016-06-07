bf239650575906c02d4bdce693c6c4eee188916a
batura97 <nik.batura.97@mail.ru>
2016-06-08 00:20:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <windows.h>
-#include <glut.h>  
+#include <GL/freeglut.h>
 #include "painter.h"
 #include "game.h"
 #include <iostream>
@@ -9,7 +9,13 @@ Game game;
 void Display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	game.Draw();
+	if (game.stateGame == 0)
+		game.Draw();
+	else if (game.stateGame == 1)
+		game.DrawGameOver("Game Over!", 15);
+	else
+		game.DrawGameOver("Win!", -5);
+	std::cout << game.points << " ";
 	glutSwapBuffers();
 }
 
@@ -19,10 +25,10 @@ void Mouse(int button, int state, int x, int y)
 	{
 		switch (button) {
 		case GLUT_LEFT_BUTTON:
-			game.Open(x / 16, y / 16);
+			game.Open(x / Game::FIELD_WIDTH, y / Game::FIELD_HEIGHT);
 			break;
 		case GLUT_RIGHT_BUTTON:
-			game.Mark(x / 16, y / 16);
+			game.Mark(x / Game::FIELD_WIDTH, y / Game::FIELD_HEIGHT);
 			break;
 		}
 		glutPostRedisplay();
@@ -33,13 +39,13 @@ int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowSize(Game::FieldWidth * Painter::CellSize, Game::FieldHeight * Painter::CellSize);
-	glutInitWindowPosition(Game::FieldWidth * Painter::CellSize, Game::FieldHeight * Painter::CellSize);
+	glutInitWindowSize(Game::FIELD_WIDTH * Painter::CELL_SIZE, Game::FIELD_HEIGHT * Painter::CELL_SIZE);
+	glutInitWindowPosition(Game::FIELD_WIDTH * Painter::CELL_SIZE / 2, Game::FIELD_HEIGHT * Painter::CELL_SIZE / 2);
 	glutCreateWindow("Сапёр");
 	glClearColor(0, 0, 0, 1);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0, Game::FieldWidth * Painter::CellSize, Game::FieldHeight * Painter::CellSize, 0, -1.0, 1.0);
+	glOrtho(0, Game::FIELD_WIDTH * Painter::CELL_SIZE, Game::FIELD_HEIGHT * Painter::CELL_SIZE, 0, -1.0, 1.0);
 	glutDisplayFunc(Display);
 	glutMouseFunc(Mouse);
 	glutMainLoop();
