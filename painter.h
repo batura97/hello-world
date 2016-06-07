4fa67d4f2457040f334eee30898235d4ebc8d917
batura97 <nik.batura.97@mail.ru>
2016-06-08 00:22:28 +0300

    Update painter.h

--- painter.h
+++ painter.h
@@ -3,12 +3,12 @@
 class Painter
 {
 public:
-	static const int CellSize = 16;
-	void DrawClosedCell(int x, int y); //Ðèñîâàíèå çàêðûòîé êëåòêè
-	void DrawPressedCell(int x, int y); //Ðèñîâàíèå íàæàòîé êëåòêè
-	void DrawOpenedCell(int x, int y, int MinesAround); //Ðèñîâàíèå îòêðûòîé êëåòêè
-	void DrawFlag(int x, int y); //Ðèñîâàíèå ôëàãà
-	void DrawMine(int x, int y); //Ðèñîâàíèå ìèíû
+	static const int CELL_SIZE = 16;
+	void DrawClosedCell(int x, int y); //Рисование закрытой клетки
+	void DrawOpenedCell(int x, int y, int MinesAround); //Рисование открытой клетки
+	void DrawFlag(int x, int y); //Рисование флага
+	void DrawMine(int x, int y); //Рисование мины
+	void DrawComplete(char *text, int x, int y, int z, int u, int i, int j, int k); //Конец игры
 private:
-	void DrawOpenedCell(int x, int y); //Ðèñîâàíèå îòêðûòêîé êëåòêè.Private, ïîòîìó ÷òî èñïîëüçóåòñÿ òîëüêî âíóòðè ïðîãðàììû
+	void DrawOpenedCell(int x, int y); //Рисование открыткой клетки.Private, потому что используется только внутри программы
 };
