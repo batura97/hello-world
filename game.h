2605f4683002b404cee14fcfbef382e138c8b2f0
batura97 <nik.batura.97@mail.ru>
2016-06-08 00:18:31 +0300

    Update game.h

--- game.h
+++ game.h
@@ -5,15 +5,22 @@ class Game
 public:
 	Game();
 	void Draw();
-	enum {FieldWidth = 9, FieldHeight = 9};
+	void DrawGameOver(char *text, int x);
+	enum {FIELD_WIDTH = 16, FIELD_HEIGHT = 16};
 	enum State { CLOSED, OPENED, FLAG};
+	enum StateGame { PLAY, GAME_OVER, WIN};
+	StateGame stateGame;
+	static const int NUMBER_OF_MINES = 40;
+	int points = 0;
+	unsigned int startTime;
+	unsigned int endTime;
 	struct Cell
 	{
 		State state;
 		bool HasMine;
 		int MinesAround;
 	};
-	Cell Field[FieldWidth][FieldHeight];
+	Cell Field[FIELD_WIDTH][FIELD_HEIGHT];
 	void Open(int x, int y);
 	void Mark(int x, int y);
 	void OpenEmptyCells(int x, int y);
